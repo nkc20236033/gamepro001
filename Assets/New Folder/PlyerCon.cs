1627411036382b062c7d73c00594dfc34b5d86b9
DESKTOP-MH3M7B4\user <nkc23.ichioka.yuta@gmail.com>
2023-06-01 10:34:52 +0900

    6/1更新

--- Assets/New Folder/PlyerCon.cs
+++ Assets/New Folder/PlyerCon.cs
@@ -19,20 +19,15 @@ public class PlyerCon : MonoBehaviour
 
     void Update()
     {
-        float speed = 0.05f;
+        float speed = 0.1f;
         float px = Input.GetAxisRaw("Horizontal");
         float py = Input.GetAxisRaw("Vertical");
         transform.position += new Vector3(px * speed, py * speed, 0);
 
-        transform.position =new Vector3
-            ( //ƒGƒŠƒAŽw’è‚µ‚ÄˆÚ“®‚·‚é
-             Mathf.Clamp( transform.position.x + px, -10.0f, 10.0f ),
-             Mathf.Clamp( transform.position.y + py, -4.5f, 4.5f )
-             );
        
         if (py == 0)
         {
-            anim.Play("Player");
+            anim.Play("player Animation");
         }
         else if (py == 1)
         {
